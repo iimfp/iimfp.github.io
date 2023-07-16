516aa45dbd8a8acc88d35fd0458f417078894d9b
Yiyu Zhou <yiyuzhou19@gmail.com>
2023-07-16 14:42:08 -0700

    Revert "Add WorldTimeAPI"
    This reverts commit f135e3f0c0a2c5c0b2f2a020487749a23109bfa6.

--- script.js
+++ script.js
@@ -26,33 +26,24 @@
  */
 
 function updateTime() {
-  const apiUrl = 'http://worldtimeapi.org/api/timezone/America/Los_Angeles';
+  let startDate = new Date('December 11, 2022 00:00:00');
+  let currentDate = new Date();
+  let timeDiff = currentDate.getTime() - startDate.getTime();
+  let seconds = Math.floor(timeDiff / 1000);
+  let minutes = Math.floor(seconds / 60);
+  let hours = Math.floor(minutes / 60);
+  let days = Math.floor(hours / 24);
 
-  fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-      const startDate = new Date('December 11, 2022 00:00:00');
-      const currentDateUnix = data.unixtime;
-      const timeDiff = (currentDateUnix * 1000) - startDate.getTime();
-      let seconds = Math.floor(timeDiff / 1000);
-      let minutes = Math.floor(seconds / 60);
-      let hours = Math.floor(minutes / 60);
-      let days = Math.floor(hours / 24);
+  seconds %= 60;
+  minutes %= 60;
+  hours %= 24;
 
-      seconds %= 60;
-      minutes %= 60;
-      hours %= 24;
+  document.getElementById('days').textContent = days;
+  document.getElementById('hours').textContent = hours;
+  document.getElementById('minutes').textContent = minutes;
+  document.getElementById('seconds').textContent = seconds;
 
-      document.getElementById('days').textContent = days;
-      document.getElementById('hours').textContent = hours;
-      document.getElementById('minutes').textContent = minutes;
-      document.getElementById('seconds').textContent = seconds;
-
-      setTimeout(updateTime, 1000);
-    })
-    .catch(error => {
-      console.log('An error occurred:', error);
-    });
+  setTimeout(updateTime, 1000);
 }
 
 window.onload = updateTime;
